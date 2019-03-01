d67d96ca6127039e6fcadf9f387db70354fcd767
Khamitsevich Mikhail <mikhail.khamitsevich@sap.com>
2019-03-01 11:42:02 +0000

    hw4

---
+++ web/N14/es4/garden.js
@@ -0,0 +1,82 @@
+function Plunt(name){
+	var _name = name;
+	
+	this.description;
+	
+	this.getName = function(){
+        return _name;
+    }
+    
+    this.setName = function(name){
+        _name = name;
+        return this;
+    }
+    
+    this.setDescription = function(description){
+        this.description = description;
+        return this;
+    }
+    
+	this.toString = function(){
+		return "Plant (super): " + 
+		"name: " + _name;
+	}
+}
+
+function Fern(name){
+	Plunt.apply(this, arguments);
+	
+	var _application;
+	
+	this.getApplication = function(){
+        return _application;
+    }
+    
+    this.setApplication = function(application){
+        _application = application;
+        return this;
+    }
+	
+	this.toString = function(){
+		return "Fern (child): " + 
+		"name: " + this.getName() + 
+		", application: " + _application;
+	}	
+	
+}
+
+function Spruce(name, description){
+	Plunt.apply(this, arguments);
+	
+	this.description = description;
+	
+	var _class;
+	
+	this.getClass = function(){
+        return _class;
+    }
+    
+    this.setClass = function(__class){
+        _class = __class;
+        return this;
+    }
+    
+    this.toString = function(){
+		return "Spruce (child): " + 
+		"name: " + this.getName() + 
+		", class: " + _class;
+	}
+    
+}
+
+function main(){
+	var plunt = new Plunt("MyPlant").setDescription("PlantDescr"); 
+	var fern = new Fern("MyFern").setDescription("FernDescr").setApplication("MyApplication");
+	var spruce = new Spruce("MySpruce", "SpruceDescr").setClass("MyClass");
+	
+	console.log(plunt.toString() + ", json: " + JSON.stringify(plunt));
+	console.log(fern.toString() + ", json: " + JSON.stringify(fern));
+	console.log(spruce.toString() + ", json: " + JSON.stringify(spruce));
+}
+
+main();
