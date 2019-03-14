72ce5a4a59f420a5c9d3c37ddef746722c856fb4
Khamitsevich Mikhail <mikhail.khamitsevich@sap.com>
2019-03-14 09:01:26 +0000

    index - call service

--- web/js/index.js
+++ web/js/index.js
@@ -1,19 +1,81 @@
+function Entity(){
+	this.name;
+}
+Entity.prototype.setName = function(name) {
+	this.name = name;
+	return this;
+}
+Entity.prototype.getName = function() {
+	return this.name;
+}
 
+function xhttpToString(xhttp){
+	var log = "";
+	['readyState','responseText','responseXML','status','statusText'].forEach(e => {
+		log += e + ": " + xhttp[e] + "; ";
+	});
+	return log + xhttp.getAllResponseHeaders();
+}
 
 function do_create(){
-	alert("create");
+	var entity = new Entity()
+		.setName(document.getElementById("name").value);
+	console.log("client create: " +  JSON.stringify(entity));
+	
+	var xhttp = new XMLHttpRequest();
+	
+	xhttp.onreadystatechange = function() {
+		console.log("onreadystatechange: " + xhttpToString(this));
+	};
+	
+	xhttp.open("POST", "https://5vujzflxoqsehff1or-abapers-server.cfapps.eu10.hana.ondemand.com/xsjs/restService.xsjs?cmd=create", true);
+	xhttp.send(JSON.stringify(entity));
+	
 }
 
 function do_read(){
-	alert("read");
+	var entity = new Entity()
+		.setName(document.getElementById("name").value);
+	console.log("client read: " +  JSON.stringify(entity));
+	
+	var xhttp = new XMLHttpRequest();
+	
+	xhttp.onreadystatechange = function() {
+		console.log("onreadystatechange: " + xhttpToString(this));
+	};
+	
+	xhttp.onload = function() {
+		console.log("onload: " + xhttpToString(this));
+	}
+
+	xhttp.onerror = function() {
+		console.log("onerror: " + xhttpToString(this));
+	}
+	
+	xhttp.open("GET", "https://5vujzflxoqsehff1or-abapers-server.cfapps.eu10.hana.ondemand.com/xsjs/restService.xsjs?cmd=read&name=" + entity.getName(), true);
+	xhttp.send();
+	
 }
 
 function do_update(){
-	alert("update");
+	var entity = new Entity()
+		.setName(document.getElementById("name").value);
+	console.log("client update: " +  JSON.stringify(entity));
 }
 
 function do_delete(){
-	alert("delete");
+	var entity = new Entity()
+		.setName(document.getElementById("name").value);
+	console.log("client delete: " +  JSON.stringify(entity));
+	
+	var xhttp = new XMLHttpRequest();
+	
+	xhttp.onreadystatechange = function() {
+		console.log("onreadystatechange: " + xhttpToString(this));
+	};
+	
+	xhttp.open("DELETE", "https://5vujzflxoqsehff1or-abapers-server.cfapps.eu10.hana.ondemand.com/xsjs/restService.xsjs", true);
+	xhttp.send();
 }
 
 (function init(){
@@ -42,8 +104,9 @@ function do_delete(){
 					
 				default:
 					throw "unknown command";
+	    	}
 		});
 		
 	});
 	
-)();
+})();
